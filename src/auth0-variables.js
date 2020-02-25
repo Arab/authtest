286d01368650cd5ccde3be4a3ee5ed12761bff5b
Piotr <pmotak@gmail.com>
2020-02-25 20:51:15 +0100

    simple rbac auth0 app can use it to build some boilerpalte if needed

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "dev-oqkr6mjb.eu.auth0.com",
+  roleUrl: "https://rbac-tutorial-app/role",
+  clientId: "986sxYcBMm6a7k0t5KM8NT2KnnxAPpoQ",
+  callbackUrl: "http://localhost:3004/callback"
+};
---
+++ src/posts.js
@@ -0,0 +1,24 @@
+const posts = [
+  {
+    id: "a",
+    title: "Post a",
+    ownerId: "auth0|5e557a0937de640d5dd42b40"
+  },
+  {
+    id: "b",
+    title: "Post b",
+    ownerId: ""
+  },
+  {
+    id: "c",
+    title: "Post c",
+    ownerId: "auth0|5e557a0937de640d5dd42b40"
+  },
+  {
+    id: "d",
+    title: "Post d",
+    ownerId: "auth0|5e557a67fa29e10d5392b267"
+  }
+];
+
+export default posts;
